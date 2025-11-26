5f56ebecdfd5261a3cb71d3a69d6a2e5c07c9c74
Frédéric S. <fsallon@cfe.fr>
2025-11-26 13:42:09 +0100

    Après install de npm vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import gitlabLogo from './assets/gitlab.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -14,25 +15,18 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <a href="https://gitlab.com" target="_blank">
-          <img src={gitlabLogo} className="logo" alt="GitLab logo" />
-        </a>
       </div>
-      <h1>Learn GitLab CI/CD</h1>
+      <h1>Vite + React</h1>
       <div className="card">
-        <h2>Welcome to the GitLab CI/CD world.</h2>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
-          Dive into the world of continuous integration and continuous delivery (CI/CD) by deploying this simple app with GitLab. Whether you’re a developer, a project manager, or just curious about DevOps practices, this course will guide you through the fundamentals of GitLab CI/CD.
-        </p>
-        <p>
-          Hint: Edit <code>src/App.jsx</code> to make chnages to this page.
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Created by Valentin Despa. 
-      </p>
-      <p className="read-the-docs">
-        Application version: 1
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build'
-  },
-  test: {
-    environment: 'jsdom',
-    setupFiles: './tests/setup.js',
-    exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/e2e/**'
-    ],
-    reporters: ['verbose', 'junit', 'html'],
-    outputFile: {
-      junit: './reports/junit.xml',
-      html: './reports/html/index.html'
-    }
-  }
 })
